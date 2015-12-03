df0051e38a93bf40df3a940b8a6791d913c8a1ce
Felix Bünemann <buenemann@louis.info>
2015-12-03 23:01:20 +0100

    v0.2.0: Use posix-spawn gem with fallback to open3
    This will reduce overhead of forking especially on linux, because posix
    spawn does not need to copy the process memory. To use this you have to
    add the gem 'posix-spawn' to your Gemfile. If posix-spawn cannot be
    loaded, the code will fallback to using open3.

--- lib/qpdf_utils/shell_runner.rb
+++ lib/qpdf_utils/shell_runner.rb
@@ -1,8 +1,13 @@
 # encoding: utf-8
-require 'open3'
+begin
+  require 'posix/spawn'
+rescue LoadError
+  require 'open3'
+end
 
 module QPDFUtils
   class ShellRunner
+
     def initialize(binary)
       @binary = binary
     end
@@ -14,11 +19,33 @@ module QPDFUtils
 
     def run_with_output(args)
       cmd = [@binary, *args]
-      output, errors, status = Open3.capture3(*cmd)
+      output, errors, status = capture(cmd)
       unless status.exitstatus == 0 || status.exitstatus == 3 # warning
         raise CommandFailed, "command #{cmd} failed with output: #{errors.inspect}", caller
       end
       output.chomp
     end
+
+    private
+
+    if defined? ::POSIX::Spawn
+
+      def capture(cmd)
+        pid, input, output, errors = POSIX::Spawn.popen4(*cmd)
+        input.close
+        _, status = Process.waitpid2(pid)
+        [output.read, errors.read, status]
+      ensure
+        [input, output, errors].each { |io| io.close unless io.closed? }
+      end
+
+    else
+
+      def capture(cmd)
+        Open3.capture3(*cmd)
+      end
+
+    end
+
   end
 end
--- lib/qpdf_utils/version.rb
+++ lib/qpdf_utils/version.rb
@@ -1,3 +1,3 @@
 module QPDFUtils
-  VERSION = "0.1.2"
+  VERSION = "0.2.0"
 end
